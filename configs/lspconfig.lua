e2e453c45e34adba2dc8fc57c20cf11824f69a13
Sefer <46936258+seferergun@users.noreply.github.com>
2023-07-03 23:39:34 +0100

    Update lspconfig.lua

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,20 +1,27 @@
---local on_attach = require("plugins.configs.lspconfig").on_attach
---local capabilities = require("plugins.configs.lspconfig").capabilities
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
 
---local lspconfig = require("lspconfig")
---local util = require "lspconfig/util"
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+local servers = { "html", "cssls", "clangd", "lua_ls", "omnisharp" }
 
 
---[[lspconfig.rust_analyzer.setup({
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+
+lspconfig.gdscript.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"rust"},
-  root_dir = util.root_pattern("Cargo.toml"),
-  settings = {
-    ['rust-analyzer'] = {
-      cargo = {
-        allFeatures = true,
-      }
-    }
+  flags = {
+    debounce_text_changes = 150,
   }
-})]]--
+}
